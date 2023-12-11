4c56f5108cf2038fe4d9ce8c06f714823f3bc561
NikaEliashvili <nikaeliashvili18@outlook.com>
2023-12-11 23:00:36 +0400

    commented faild import

--- app/(platform)/(dashboard)/organization/[organizationId]/billing/_components/subscription-button.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/billing/_components/subscription-button.tsx
@@ -4,7 +4,7 @@ import { toast } from "sonner";
 
 import { useAction } from "@/hooks/use-action";
 import { Button } from "@/components/ui/button";
-import { stripeRedirect } from "@/actions/stripe-redirect";
+// import { stripeRedirect } from "@/actions/stripe-redirect";
 import { useProModal } from "@/hooks/use-pro-modal";
 
 interface SubscriptionButtonProps {
@@ -16,25 +16,29 @@ export const SubscriptionButton = ({
 }: SubscriptionButtonProps) => {
   const proModal = useProModal();
 
-  const { execute, isLoading } = useAction(stripeRedirect, {
-    onSuccess: (data) => {
-      window.location.href = data;
-    },
-    onError: (error) => {
-      toast.error(error);
-    },
-  });
+  // const { execute, isLoading } = useAction(stripeRedirect, {
+  //   onSuccess: (data) => {
+  //     window.location.href = data;
+  //   },
+  //   onError: (error) => {
+  //     toast.error(error);
+  //   },
+  // });
 
   const onClick = () => {
     if (isPro) {
-      execute({});
+      // execute({});
     } else {
       proModal.onOpen();
     }
   };
 
   return (
-    <Button variant="primary" onClick={onClick} disabled={isLoading}>
+    <Button
+      variant="primary"
+      onClick={onClick}
+      // disabled={isLoading}
+    >
       {isPro ? "Manage subscription" : "Upgrade to pro"}
     </Button>
   );
