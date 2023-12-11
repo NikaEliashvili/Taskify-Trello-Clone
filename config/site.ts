a2478594b99a5d2f3e9c56ea7a8b870effb40ce6
NikaEliashvili <nikaeliashvili18@outlook.com>
2023-12-11 23:25:02 +0400

    nothing

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
   name: "Taskify",
   description:
-    "Collaborate, manage projects, and reach new productivity peaks.",
+    "Collaborate, manage projects, and reach new productivity peaks. ",
 };
